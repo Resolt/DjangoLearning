b5201f3aadfa6f58e8c6563f544c25c1ff8ecbe8
resolt <pdmmichaelsen@gmail.com>
2020-01-16 23:13:01 +0100

    -outline of some site is done -heavy debugging still needed as nothing has been tested or asured to be properly connected so far ie. nothing works

--- django/someproject/someproject/urls.py
+++ django/someproject/someproject/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('posts/', include('posts.urls', namespace="posts")),
+    path('groups/', include('groups.urls', namespace="groups")),
     path('test', main_views.TestPage.as_view(), name="test"),
     path('thanks', main_views.ThanksPage.as_view(), name="thanks"),
 ]
